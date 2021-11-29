ab928c39a24bb84c938f3cf185e6fb9239e41d19
subhash sharma <subhash810770yahoo.com>
2021-11-29 16:44:07 +0530

    Created  navbar

---
+++ src/component/navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link , useLocation } from "react-router-dom";
+import './Navbar.css';
+
+const Navbar = () => {
+  let location = useLocation();
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Pockedia Notes
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse mx-4" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/'? "active": "" } `} aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/about'? "active": "" } `} to="/about">
+                About 
+              </Link>
+            </li>
+          </ul>
+          <li className="d-flex">
+             <Link to="/" className="btn btn-outline-success" type="submit">
+              Login
+            </Link>
+          </li>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
